87104bea6dbadb5aeeb4e78f69660a252c039d71
Ilhan Demirer <idemirer@gmail.com>
2021-11-28 08:52:58 -0500

    change theme test 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,5 @@
 import '../styles/global.css';
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
 
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState(false);
-
-  useEffect(() => {
-    const root = document.documentElement;
-    theme
-      ? root.style.setProperty('--background', 'var(--lightBackgroundColor)') &
-        root.style.setProperty('--text', 'var(--lightTextColor)') &
-        root.style.setProperty('--link', 'var(--lightLinkColor)') &
-        root.style.setProperty('--hover', 'var(--lightHoverColor)') &
-        root.style.setProperty('--shadow', 'var(--lightShadow)') &
-        root.style.setProperty('--lightText', 'var(--lightLText)')
-      : root.style.setProperty('--background', 'var(--darkBackgroundColor)') &
-        root.style.setProperty('--text', 'var(--darkTextColor)') &
-        root.style.setProperty('--link', 'var(--darkLinkColor)') &
-        root.style.setProperty('--hover', 'var(--darkHoverColor)') &
-        root.style.setProperty('--shadow', 'var(--darkShadow)') &
-        root.style.setProperty('--lightText', 'var(--lightDText)');
-  });
-
-  const onClick = () => {
-    setTheme(!theme);
-  };
   return <Component {...pageProps} />;
 }
