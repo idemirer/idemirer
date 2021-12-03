b54f3adf761acb5269190794e618d7a46978af1a
Ilhan Demirer <idemirer@gmail.com>
2021-12-03 09:32:12 -0500

    broken link

--- pages/posts/tags/[tag].js
+++ pages/posts/tags/[tag].js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Layout from '../../../components/layout';
-import utilStyles from '../../styles/utils.module.css';
+import utilStyles from '../../../styles/utils.module.css';
 import { getAllPostTags, getSortedPostsData, searchTags } from '../../../lib/posts';
 import PostListing from '../../../components/listposts';
 
