3f73571d5f503b279d3905749d3a1e05d5775ec0
Ilhan Demirer <idemirer@gmail.com>
2022-02-03 19:29:05 -0500

    weekly update

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -5,7 +5,7 @@ import CreateChart from '../../components/apexchartlayout';
 import strdata from '../../assets/data/str.json';
 import tsaData from '../../assets/data/passengerData.json';
 
-const updateDate = 'Jan 27, 2022';
+const updateDate = 'Feb 4, 2022';
 
 let occData2019 = [];
 let occData2020 = [];
