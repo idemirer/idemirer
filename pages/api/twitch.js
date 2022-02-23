77ab6129f192d35f782fa2aac9c7f14bb020c1e4
Ilhan Demirer <idemirer@gmail.com>
2022-02-23 11:03:48 -0500

    twitch api

--- pages/api/twitch.js
+++ pages/api/twitch.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-// async function getTwitchOAuth() {
-//   const url = `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
-//   const twitchOAuth = await axios.post(url);
-//   return twitchOAuth.data.access_token;
-// }
+async function getTwitchOAuth() {
+  const url = `https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
+  const twitchOAuth = await axios.post(url);
+  return twitchOAuth.data.access_token;
+}
 
 export async function getTwitchStatus() {
   const searchURL = 'https://api.twitch.tv/helix/search/channels?query=impostorengineer';
+  let auth = getTwitchOAuth();
   const searchData = await axios
     .get(searchURL, {
       headers: {
-        Authorization: `Bearer ${process.env.TWITCH_AUTHORIZATION}`,
+        Authorization: `Bearer ${auth}`,
         'Client-Id': process.env.TWITCH_CLIENT_ID,
       },
     })
