48da07ef914bad733ffaef92085c137415ba539f
Ilhan Demirer <idemirer@gmail.com>
2022-03-16 12:58:35 -0400

    fix 2 google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,10 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TRACKING_ID}`} />
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-JP5LKKYL7V' />
       <Script
+        id='google-analytics'
+        strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || []; 
           function gtag(){dataLayer.push(arguments);} 
