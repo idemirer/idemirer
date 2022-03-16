a52c2134adc752b06956dc1581ebf3195990bf36
Ilhan Demirer <idemirer@gmail.com>
2022-03-16 12:42:55 -0400

    update googleanalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '../styles/global.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TRACKING_ID}`} />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date());
+          gtag('config', '${process.env.GOOGLE_TRACKING_ID}', {page_path: window.location.pathname,});`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,11 @@ import Layout, { siteTitle } from '../components/layout';
 import Hero from '../components/hero';
 import LatestPosts from '../components/latestposts';
 import { getSortedPostsData } from '../lib/posts';
-import Script from 'next/script';
 
 const Home = ({ allPostsData }) => {
   return (
     <Layout home>
       <Head>
-        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TRACKING_ID}`} />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || []; 
-          function gtag(){dataLayer.push(arguments);} 
-          gtag('js', new Date());
-          gtag('config', '${process.env.GOOGLE_TRACKING_ID}', {page_path: window.location.pathname,});`,
-          }}
-        />
         <title>{siteTitle}</title>
       </Head>
       <Hero />
