d169b8460922d378af6df1cdbf398043c7fe7a82
Ilhan Demirer <idemirer@gmail.com>
2022-03-24 21:18:01 -0400

    post & data update

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -5,7 +5,7 @@ import CreateChart from '../../components/apexchartlayout';
 import strdata from '../../assets/data/str.json';
 import tsaData from '../../assets/data/passengerData.json';
 
-const updateDate = 'Mar 18, 2022';
+const updateDate = 'Mar 24, 2022';
 
 let occData2019 = [];
 let occData2020 = [];
