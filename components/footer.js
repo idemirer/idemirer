ca8aa3df33f19b5bad0b1800014d13319402b3ab
Ilhan Demirer <idemirer@gmail.com>
2022-11-02 16:50:00 -0400

    update footer

--- components/footer.js
+++ components/footer.js
@@ -1,54 +1,79 @@
-import utilStyles from '../styles/utils.module.css';
+import utilStyles from '../styles/utils.module.css'
 
 export default function Footer() {
   return (
     <div className={utilStyles.footer}>
       <div>
-        <a className={utilStyles.email} href='mailto:idemirer@gmail.com' alt='email' target='_blank'>
-          <i aria-hidden className='fa-solid fa-envelope'></i>
+        <a
+          className={utilStyles.email}
+          href="mailto:idemirer@gmail.com"
+          alt="email"
+          target="_blank"
+        >
+          <i aria-hidden className="fa-solid fa-envelope"></i>
         </a>
       </div>
       <div>
         <a
           className={utilStyles.googleScholar}
-          href='https://scholar.google.com/citations?user=CzJ07gwAAAAJ'
-          alt='Google Scholar'
-          target='_blank'
+          href="https://scholar.google.com/citations?user=CzJ07gwAAAAJ"
+          alt="Google Scholar"
+          target="_blank"
         >
-          <i aria-hidden className='fa-solid fa-graduation-cap'></i>
+          <i aria-hidden className="fa-solid fa-graduation-cap"></i>
         </a>
       </div>
       <div>
         <a
           className={utilStyles.researchGate}
-          href='https://www.researchgate.net/profile/Ilhan-Demirer-2'
-          alt='ResearchGate'
-          target='_blank'
+          href="https://www.researchgate.net/profile/Ilhan-Demirer-2"
+          alt="ResearchGate"
+          target="_blank"
         >
-          <i aria-hidden className='fa-brands fa-researchgate'></i>
+          <i aria-hidden className="fa-brands fa-researchgate"></i>
         </a>
       </div>
       <div>
         <a
           className={utilStyles.linkedin}
-          href='https://www.linkedin.com/in/ilhan-demirer-3230642a/'
-          alt='LinkedIn'
-          target='_blank'
+          href="https://www.linkedin.com/in/ilhan-demirer-3230642a/"
+          alt="LinkedIn"
+          target="_blank"
         >
-          <i aria-hidden className='fab fa-linkedin'></i>
+          <i aria-hidden className="fab fa-linkedin"></i>
         </a>
       </div>
       <div>
-        <a className={utilStyles.twitter} href='https://twitter.com/idemirer' alt='twitter' target='_blank'>
-          <i aria-hidden className='fab fa-twitter'></i>
+        <a
+          className={utilStyles.email}
+          href="https://www.youtube.com/@IlhanDemirer"
+          alt="YouTube"
+          target="_blank"
+        >
+          <i aria-hidden class="fa-solid fa-video"></i>
         </a>
       </div>
       <div>
-        <a className={utilStyles.github} href='https://github.com/idemirer' alt='github' target='_blank'>
-          <i aria-hidden className='fab fa-github'></i>
+        <a
+          className={utilStyles.twitter}
+          href="https://twitter.com/idemirer"
+          alt="twitter"
+          target="_blank"
+        >
+          <i aria-hidden className="fab fa-twitter"></i>
+        </a>
+      </div>
+      <div>
+        <a
+          className={utilStyles.github}
+          href="https://github.com/idemirer"
+          alt="github"
+          target="_blank"
+        >
+          <i aria-hidden className="fab fa-github"></i>
         </a>
       </div>
       <div className={utilStyles.lightText}>Copyright &copy; Ilhan Demirer 2021</div>
     </div>
-  );
+  )
 }
