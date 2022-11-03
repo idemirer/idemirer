8b03eb3d928b68c414db9fa2d58bc52a99cf7630
Ilhan Demirer <idemirer@gmail.com>
2022-11-03 01:08:08 -0400

    fix css 3

--- components/postBody.js
+++ components/postBody.js
@@ -1,55 +1,55 @@
-import * as React from 'react';
-import Image from 'next/image';
-import { unified } from 'unified';
-import reactRehyped from 'rehype-react';
-import rehypeParse from 'rehype-parse';
-import utilStyles from '../styles/utils.module.css';
-import Zoom from 'react-medium-image-zoom';
+import * as React from 'react'
+import Image from 'next/image'
+import { unified } from 'unified'
+import reactRehyped from 'rehype-react'
+import rehypeParse from 'rehype-parse'
+import utilStyles from '../styles/utils.module.css'
+import Zoom from 'react-medium-image-zoom'
 
-const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+const keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
 
 const triplet = (e1, e2, e3) =>
   keyStr.charAt(e1 >> 2) +
   keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
   keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-  keyStr.charAt(e3 & 63);
+  keyStr.charAt(e3 & 63)
 
 const rgbDataURL = (r, g, b) =>
   `data:image/gif;base64,R0lGODlhAQABAPAA${
     triplet(0, r, g) + triplet(b, 255, 255)
-  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`
 
 const components = {
   html: ({ children }) => <>{children}</>,
   body: ({ children }) => <>{children}</>,
   head: ({ children }) => <>{children}</>,
   img: (imgProps) => {
-    const { src, ...restOfImgProps } = imgProps;
+    const { src, ...restOfImgProps } = imgProps
     if (src.startsWith('http')) {
       return (
         <Zoom>
           <img src={src} {...restOfImgProps} className={utilStyles.postImage} />
         </Zoom>
-      );
+      )
     }
     return (
-      <Zoom>
+      <Zoom classDialog="custom-zoom">
         <Image
           className={utilStyles.postImage}
-          placeholder='blur'
+          placeholder="blur"
           blurDataURL={rgbDataURL(102, 102, 102)}
           src={src}
           {...restOfImgProps}
         />
       </Zoom>
-    );
+    )
   },
-};
+}
 
 export default function PostBody({ content }) {
   const contentMD = unified()
     .use(rehypeParse)
     .use(reactRehyped, { createElement: React.createElement, components: components })
-    .processSync(content).result;
-  return <>{contentMD}</>;
+    .processSync(content).result
+  return <>{contentMD}</>
 }
