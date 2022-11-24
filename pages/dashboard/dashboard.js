c670894da0456308c63090d2b2caf13f45c86242
Ilhan Demirer <idemirer@gmail.com>
2022-11-24 10:09:27 -0500

    post #47 data

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -11,7 +11,7 @@ for (let i = 0; i < 120; i++) {
   tsaData['data'].push(tsaRawData['data'][i]);
 }
 
-const updateDate = 'November 18, 2022';
+const updateDate = 'November 24, 2022';
 
 let occData2019 = [];
 let occData2020 = [];
