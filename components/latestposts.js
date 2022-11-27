29eece0cbc953cec8db9a611ece926226170bb74
Ilhan Demirer <idemirer@gmail.com>
2022-11-27 12:01:33 -0500

    added draft tag

--- components/latestposts.js
+++ components/latestposts.js
@@ -1,15 +1,12 @@
 import utilStyles from '../styles/utils.module.css';
 import PostListing from './listposts';
-import { parseISO } from 'date-fns';
-
-const todaysDate = new Date();
 
 const LatestPosts = ({ data }) => {
   return (
     <section className={`${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingMd}>Latest blog posts:</h2>
       <div className={utilStyles.posts}>
-        <PostListing postData={data.filter(({ id, date, title, tags }) => todaysDate >= parseISO(date)).slice(0, 2)} />
+        <PostListing postData={data.slice(0, 2)} />
       </div>
     </section>
   );
--- components/listposts.js
+++ components/listposts.js
@@ -2,46 +2,42 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import PostDate from './formattedDate';
 import Image from 'next/image';
-import { parseISO } from 'date-fns';
 
-const todaysDate = new Date();
 const PostListing = ({ postData }) => {
   return (
     <>
-      {postData
-        .filter(({ id, date, title, tags, banner }) => todaysDate >= parseISO(date))
-        .map(({ id, date, title, tags, banner }) => (
-          <div className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>
-                <Image
-                  className={utilStyles.banner}
-                  src={`/images/blogimages/${banner}`}
-                  alt='banner'
-                  height={150}
-                  width={380}
-                  objectFit='cover'
-                  objectPosition='left top'
-                />{' '}
-                <br />
-                <div className={utilStyles.listItemText}>{title}</div>
-              </a>
-            </Link>
-            <div className={utilStyles.listItemText}>
-              {tags.map((item, key) => (
-                <small className={utilStyles.tagsWrapper} key={key}>
-                  <Link href={`/posts/tags/${item}`}>
-                    <a className={`${utilStyles.tags} ${utilStyles.lightText}`}>{`#${item}`}</a>
-                  </Link>
-                </small>
-              ))}
+      {postData.map(({ id, date, title, tags, banner }) => (
+        <div className={utilStyles.listItem} key={id}>
+          <Link href={`/posts/${id}`}>
+            <a>
+              <Image
+                className={utilStyles.banner}
+                src={`/images/blogimages/${banner}`}
+                alt='banner'
+                height={150}
+                width={380}
+                objectFit='cover'
+                objectPosition='left top'
+              />{' '}
               <br />
-              <small className={utilStyles.lightText}>
-                <PostDate dateString={date} />
+              <div className={utilStyles.listItemText}>{title}</div>
+            </a>
+          </Link>
+          <div className={utilStyles.listItemText}>
+            {tags.map((item, key) => (
+              <small className={utilStyles.tagsWrapper} key={key}>
+                <Link href={`/posts/tags/${item}`}>
+                  <a className={`${utilStyles.tags} ${utilStyles.lightText}`}>{`#${item}`}</a>
+                </Link>
               </small>
-            </div>
+            ))}
+            <br />
+            <small className={utilStyles.lightText}>
+              <PostDate dateString={date} />
+            </small>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
 };
