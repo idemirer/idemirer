5aa7fac93bfd490b1f35ad0e99f9ddb0ba920305
Ilhan Demirer <idemirer@gmail.com>
2022-12-09 12:13:23 -0500

    add inter font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global.css';
 import Script from 'next/script';
+import '@fontsource/inter/variable-full.css';
 
 export default function App({ Component, pageProps }) {
   return (
