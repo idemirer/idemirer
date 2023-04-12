8b9ec5449b950f7dc00f8a3ecc72a3f5b17116a9
Ilhan Demirer <idemirer@gmail.com>
2023-04-12 10:48:57 -0400

    fix layout issues

--- components/layout.js
+++ components/layout.js
@@ -1,38 +1,12 @@
 import styles from './layout.module.css';
 import Navbar from './navbar';
 import Footer from './footer';
-import Head from 'next/head';
 
 export const siteTitle = 'Ilhan Demirer Personal Website';
 
 export default function Layout({ children, home, post }) {
   return (
     <>
-      <Head>
-        <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/images/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/images/favicon-16x16.png' />
-        <link rel='shortcut icon' href='/images/favicon.ico' />
-        <link rel='mask-icon' href='/images/safari-pinned-tab.svg' color='#5bbad5' />
-        <link rel='manifest' href='/images/site.webmanifest' />
-        <meta name='msapplication-config' content='/images/browserconfig.xml' />
-        <meta name='theme-color' content='#ffffff' />
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-        <meta name='description' content='Management faculty, share news and data on this site.' />
-        <meta itemProp='name' content='Ilhan Demirer Personal Website' />
-        <meta itemProp='description' content='Management faculty, share news and data on this site.' />
-        <meta itemProp='image' content='https://www.ilhandemirer.com/images/wide-card.png' />
-        <meta property='og:url' content='https://www.ilhandemirer.com' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Ilhan Demirer Personal Website' />
-        <meta property='og:description' content='Management faculty, share news and data on this site.' />
-        <meta property='og:image' content='https://www.ilhandemirer.com/images/wide-card.png' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:title' content='Ilhan Demirer Personal Website' />
-        <meta name='twitter:description' content='Management faculty, share news and data on this site.' />
-        <meta name='twitter:image' content='https://www.ilhandemirer.com/images/wide-card.png' />
-      </Head>
       <div className={styles.container}>
         <header className={styles.header}>
           <Navbar />
