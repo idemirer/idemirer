2019ff8efbd3ce69c953833ec299d50950ea2a20
Ilhan Demirer <idemirer@gmail.com>
2024-10-16 15:13:16 -0400

    post #42 updates

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -5,7 +5,7 @@ import CreateChart from '../../components/apexchartlayout';
 import strData from '../../assets/data/strdata.json';
 import tsaRawData from '../../assets/data/passengerData.json';
 
-const updateDate = 'Oct 11, 2024';
+const updateDate = 'Oct 18, 2024';
 
 let tsaChartSourceData = tsaRawData['data'].slice(0, 90).reduce((obj, days) => {
   const years = ['2022', '2023', '2024', 'date'];
