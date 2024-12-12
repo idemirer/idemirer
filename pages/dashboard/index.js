0dba0db3f4b9f71299f84e58011be35c033b55b4
Ilhan Demirer <idemirer@gmail.com>
2024-12-12 15:13:27 -0500

    fix date

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -6,7 +6,7 @@ import strData from '../../assets/data/strdata.json';
 import tsaRawData from '../../assets/data/passengerData.json';
 
 export default function Dashboard() {
-  const updateDate = 'Dec 6, 2024';
+  const updateDate = 'Dec 12, 2024';
 
   let tsaChartSourceData = tsaRawData['data'].slice(0, 90).reduce((obj, days) => {
     const years = ['2022', '2023', '2024', 'date'];
